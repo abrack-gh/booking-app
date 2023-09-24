519fccdfd76df87d75e90393ea8a4ea8a705e697
abrack-gh <a.brack@live.co.uk>
2023-09-24 16:16:33 +0100

    var and const

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 
 	var conferenceName = "Learn Go Conference"
-	fmt.Println("Welcome to our"+conferenceName, "lesson booking application!")
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to our", conferenceName, "lesson booking application!")
+	fmt.Println("There are currently", remainingTickets, "remaining tickets out of a total of", conferenceTickets)
 
 }
