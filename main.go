a2f7684e62ead966067e3f0968f1f4032bc47be6
abrack-gh <a.brack@live.co.uk>
2023-09-26 17:06:28 +0100

    Boolean values

--- main.go
+++ main.go
@@ -9,12 +9,10 @@ func main() {
 
 	var conferenceName = "Learn Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets uint = conferenceTickets
 
 	// fmt.Printf("conferenceTickets is %T, remaining tickets is %T, conference name is %T \n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to our %v lesson booking application!\n", conferenceName)
-	fmt.Printf("There are currently %v remaining tickets out of a total of %v. \n", remainingTickets, conferenceTickets)
 
 	for {
 
@@ -25,8 +23,10 @@ func main() {
 
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
+
 		fmt.Println("Enter your last name: ")
 		fmt.Scan(&lastName)
+
 		fmt.Println("Enter your tickets required: ")
 		fmt.Scan(&userTickets)
 
@@ -43,7 +43,7 @@ func main() {
 		fmt.Printf("First index: %v \n", bookings[0])
 		fmt.Printf("Length of slice: %v \n", len(bookings))
 
-		var ticketsLeft uint = remainingTickets - userTickets
+		var ticketsLeft uint = conferenceTickets - userTickets
 		fmt.Printf("%v tickets remaining. \n", ticketsLeft)
 
 		firstNames := []string{}
@@ -54,6 +54,13 @@ func main() {
 		}
 
 		fmt.Printf("All of our bookings: %v\n", firstNames)
+
+		if ticketsLeft == 0 {
+
+			//If tickets = 0, end application
+			fmt.Println("Tickets sold out!")
+			break
+		}
 
 	}
 
